8b76131e20da76d882a006ceff248675a63002c4
ravitejas <ravitejastech@gmail.com>
2025-08-10 00:37:21 +0530

    chore: setup boilerplate for all sections

---
+++ app/routes/about.tsx
@@ -0,0 +1,14 @@
+export default function () {
+    return (
+        <div className="min-h-screen flex items-center justify-center pt-20">
+            <div className="text-center">
+                <h1 className="text-4xl font-bold text-primary-500 mb-4">
+                    About Me
+                </h1>
+                <p className="text-gray-600 dark:text-gray-300">
+                    Coming Soon...
+                </p>
+            </div>
+        </div>
+    );
+}
