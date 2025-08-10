543dcc717071dc24e65ba155508b974be7328e26
ravitejas <ravitejastech@gmail.com>
2025-08-10 16:39:45 +0530

    feat: add toaster in root layout

--- app/root.tsx
+++ app/root.tsx
@@ -10,6 +10,7 @@ import {
 import type { Route } from "./+types/root";
 import "./app.css";
 import Header from "./components/header";
+import { Toaster } from "sonner";
 
 export const links: Route.LinksFunction = () => [
     { rel: "preconnect", href: "https://fonts.googleapis.com" },
@@ -38,6 +39,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
             </head>
             <body>
                 <Header />
+                <Toaster />
                 {children}
                 <ScrollRestoration />
                 <Scripts />
